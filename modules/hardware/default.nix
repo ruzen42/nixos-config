c2d4fe4d63c24b1bcecd9909c7f21a1d50858194
ruzen42 <malledeferlive@gmail.com>
2026-07-26 23:11:26 +0500

    fix

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -9,13 +9,7 @@
   networking.hostId = "8425e349";
   boot.zfs.forceImportRoot = false;
 
-  boot.kernelPackages = pkgs.linuxPackages_xanmod.override {
-    structuredExtraConfig = with pkgs.lib.kernel; {
-      HZ_1000 = yes;
-      HZ = freeform "1000";
-      NO_HZ_FULL = yes;
-   };
-  };
+  boot.kernelPackages = pkgs.linuxPackages_xanmod;
   boot.kernelParams = [ 
     "threadirqs"
     "preempt=full"            
