3883276ba36ac8070ba069411da4b5f446194c27
ruzen42 <malledeferlive@gmail.com>
2026-07-26 15:36:44 +0500

    feat

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -9,11 +9,19 @@
   networking.hostId = "8425e349";
   boot.zfs.forceImportRoot = false;
 
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  #boot.kernelPackages = pkgs.linuxPackages_xanmod.override {
+  #  structuredExtraConfig = with pkgs.lib.kernel; {
+  #    HZ_1000 = yes;
+  #    HZ = freeform "1000";
+  #    NO_HZ_FULL = yes;
+  # };
+  #};
+  boot.kernelPackages = pkgs.linuxPackages_cachyos;
   boot.kernelParams = [ 
     "threadirqs"
     "preempt=full"            
-    "amd_pstate=passive"       
+    "amd_pstate=active"       
+    "split_lock_detect=off"
     "usbcore.autosuspend=-1"    
     "zfs.zfs_arc_max=1610612736"
     "zfs.zfs_arc_min=536870912"
