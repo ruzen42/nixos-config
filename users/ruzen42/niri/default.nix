6f727bc0dc58b61229c2fc9055c8bed96307a737
ruzen42 <malledeferlive@gmail.com>
2026-08-11 21:25:48 +0500

    feat

--- users/ruzen42/niri/default.nix
+++ users/ruzen42/niri/default.nix
@@ -4,9 +4,8 @@
 
   programs.niri.settings = {
     spawn-at-startup = [
-      { command = [ "wall-set" "/etc/nixos/walls" ]; }
+      { command = [ "swaybg" "-i" "/etc/nixos/walls/main.jpg" ]; }
       { command = [ "mako" ]; }
-      { command = [ "gnome-keyring-daemon" "--start" "--components=secrets,pkcs11,ssh"]; }
       { command = [ "qs" ]; }
     ];
 
