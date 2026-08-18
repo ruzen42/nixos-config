870289f63d892a0a5ccf8c907880bfe0e102dc17
ruzen42 <malledeferlive@gmail.com>
2026-08-18 21:37:03 +0500

    fix(add gopls)

--- users/ruzen42/nvim/plugins/lsp.nix
+++ users/ruzen42/nvim/plugins/lsp.nix
@@ -6,6 +6,7 @@
       servers = {
         nil_ls.enable = true;     
         pyright.enable = true;   
+        gopls.enable = true;   
         rust_analyzer = {       
           enable = true;
           installRustc = false;
